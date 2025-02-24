bb48cd30ba50cffcf06214079628274689658a23
hfritz34 <henryfritz00@gmail.com>
2025-02-24 14:07:00 -0500

    vite conig change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Change output directory from 'dist' to 'build'
-  }
+  // Note: Vite uses 'dist' by default, let's keep it that way
 }) 
