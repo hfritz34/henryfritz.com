449f2f73a72c7190f0979ea5cb020e0012979a94
hfritz34 <henryfritz00@gmail.com>
2025-02-24 13:29:34 -0500

    navbar component

--- henryfritz.web/src/App.jsx
+++ henryfritz.web/src/App.jsx
@@ -2,22 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      {/* Navigation */}
-      <nav className="navbar">
-        <div className="nav-brand">Henry Fritz</div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-
+      <Navbar />
       {/* Hero Section */}
       <section id="home" className="hero">
         <h1>Hi, I'm Henry Fritz</h1>
