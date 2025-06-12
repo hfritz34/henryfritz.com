0146d8e6264af5c4bb96bec4f1059ec05a02d417
hfritz34 <henryfritz00@gmail.com>
2025-06-12 18:32:18 -0400

    cursor fixes

--- henryfritz.web/src/components/AnimatedText.jsx
+++ henryfritz.web/src/components/AnimatedText.jsx
@@ -5,25 +5,30 @@ import '../styles/AnimatedText.css';
 const AnimatedText = () => {
   return (
     <div className="animated-text-container">
-      <TypeAnimation
-        sequence={[
-          'Hi, I\'m',
-          1000,
-          'Hi, I\'m a Developer',
-          2000,
-          'Hi, I\'m an Architect',
-          2000,
-          'Hi, I\'m a Builder',
-          2000,
-        ]}
-        wrapper="span"
-        speed={30}
-        deletionSpeed={20}
-        repeat={Infinity}
-        className="animated-text"
-        cursor={true}
-        style={{ display: 'inline-block' }}
-      />
+      <div className="text-with-cursor">
+        <TypeAnimation
+          sequence={[
+            'Hi, I\'m',
+            1000,
+            'Hi, I\'m a Developer',
+            2000,
+            'Hi, I\'m an Architect',
+            2000,
+            'Hi, I\'m a Builder',
+            2000,
+            'Hi, I\'m a Creative',
+            2000,
+          ]}
+          wrapper="span"
+          speed={30}
+          deletionSpeed={20}
+          repeat={Infinity}
+          className="animated-text"
+          cursor={false}
+          style={{ display: 'inline-block' }}
+        />
+        <span className="custom-cursor">|</span>
+      </div>
       <h1 className="name-text">Henry Fritz</h1>
     </div>
   );
